9feaed43ae186515f77623e3f346a9f9ecbc22cd
Chandan95765 <chandan95765@gmail.com>
2025-03-23 15:24:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TodoListComponents from './Components/TodoListComponents'
+
+function App() {
+  const [count,setCount] = useState(0);
+  const [inputValue,setInputValue] = useState('');
+  const [list,setList] = useState([]);
+
+  function printValue()
+  {
+    setList([...list,inputValue])
+    return console.log(`${count} list is: ${inputValue}`);
+  }
+ 
+  useEffect(()=>{
+    // console.log(`${count} list is: ${inputValue}`);
+    document.title = (`Count : ${count}`);
+    printValue();
+  },[count])
+
+  return (
+    <>
+    <h1>Welcome to useEffect hook</h1>
+      <div className='todo-container'>
+      <input 
+            type="text" 
+            className='todo-input' 
+            placeholder='Enter your todo list...'
+            onChange={(e)=>{
+                setInputValue(e.target.value)
+            }}
+        />
+      <button onClick={()=>{setCount(count+1)} } className='todo-input-btn'>+</button>
+      {
+        list.map((value,index)=>(
+          <p key={index}>{value}</p>
+        ))
+      }
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/TodoListComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoListComponents = (inputValue) => {
+  return (
+    <p>TodoListComponents and {inputValue}</p>
+  )
+}
+
+export default TodoListComponents
